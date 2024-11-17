11280affaab31f83c59be319e9173f5e24add1d3
Liminghing <im349412@gmail.com>
2024-11-17 17:29:33 +0900

    working on master

--- app/src/main/java/com/example/practicegit/main.kt
+++ app/src/main/java/com/example/practicegit/main.kt
@@ -2,6 +2,6 @@ package com.example.practicegit
 
 class main {
     //안드로이드 스튜디오의 GUI를 이용한 커밋
-    //testBranch에서 작업
-    var testString ="testBranch"
+    //master에서 작업
+    var testString ="master"
 }
